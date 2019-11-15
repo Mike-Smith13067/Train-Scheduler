b91bd42cdcb2d23cfacc37a97fa086b5a8f3dcb6
Mike Smith <hockeydad66@live.com>
2019-11-14 19:11:40 -0700

    it worksgit add .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,44 +12,63 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
+
 var newTrainName = 0;
-var destination = 0;
-var trainFrequency = 0;
 
 //Add new trains to schedule
+
+//Collect user input
 
 $("#newTrainSubmit").on("click", function (event) {
     event.preventDefault();
     newTrainName = $("#newTrainName").val().trim();
     destination = $("#destination").val().trim();
-    startDate = $("#startDate").val().trim();
-    monthlyRate = $("#monthlyRate").val().trim();
+    firstTrain = $("#firstTrain").val().trim();
+    trainFreq = $("#trainFreq").val().trim();
+    console.log(newTrainName);
+    console.log(destination);
+    console.log(trainFreq);
+    var newTrain = {
+        name: newTrainName,
+        destination: destination,
+        trainFreq: trainFreq,
+        firstTrain: firstTrain,
+    }
+    console.log(newTrain);
+
+    //Push user data to database
 
     database.ref().push({
         newTrainName: newTrainName,
-        destination: destinaion,
-        startDate: startDate,
-        monthlyRate: monthlyRate,
+        destination: destination,
+        trainFreq: trainFreq,
+        firstTrain: firstTrain,
     })
-})
 
-database.ref().on("child_added", function (childSnapShot) {
-    newTrainName = childSnapShot.val().newTrainName;
-    destination = childSnapShot.val().destination;
-    // startDate = childSnapShot.val().startDate;
-    // monthlyRate = childSnapShot.val().monthlyRate;
-    // monthsWorked = startDate.moment().format("MM/DD/YYYY");
-    // convertedDate = moment(startDate, "MM/DD/YYYY");
+    database.ref().on("child_added", function (childSnapShot) {
+        newTrainName = childSnapShot.val().newTrainName;
+        destination = childSnapShot.val().destination;
+        trainFreq = childSnapShot.val().trainFreq;
+        firstTrain = childSnapShot.val().firstTrain;
+        firstTime = moment(firstTrain, "HH:mm");
+        var trainInterval = moment().diff(moment(firstTime), "minutes");
+        var trainRemain = trainInterval % trainFreq;
+        var arrivalMin = trainFreq - trainRemain;
+        var nextTrain = moment().add(arrivalMin, "m").format("hh:mm A");
+        console.log(trainInterval);
+        console.log(trainRemain);
+        console.log(arrivalMin);
+        console.log(nextTrain);
+// Display new train information
+        var newRow = $("<tr>").append(
+            $("<td>").text(newTrainName),
+            $("<td>").text(destination),
+            $("<td>").text(trainFreq),
+            $("<td>").text(nextTrain),
+            $("<td>").text(arrivalMin),
 
-    var newRow = $("<tr>");
-    newRow.append($("<td>").text(newTrainName));
-    newRow.append($("<td>").text(destination));
-    // newRow.append($("<td>").text(startDate));
-    // newRow.append($("<td>").text(monthsWorked));
-    // newRow.append($("<td>").text(monthlyRate));
-    // newRow.append($("<td>").text(totalBilledValue));
-    // Moment js
-    // var randomDate = "2/23/199";
-    // var randomFormat = "MM/DD/YYYY";
-    // var convertDate = moment(randomDate, randomFormat);
+        )
+
+        $("table > tbody").append(newRow)
+    });
 });
